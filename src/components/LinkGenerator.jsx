b7c622d023203299e0b1444d0536ca103821e95f
D4fto <163424876+D4fto@users.noreply.github.com>
2025-09-19 16:15:41 -0300

    Formatação
    Arrumei a formatação na lista de contatos e no gerador de links.

--- src/components/LinkGenerator.jsx
+++ src/components/LinkGenerator.jsx
@@ -1,28 +1,44 @@
-import { useState } from "react"
+import { useState } from "react";
 import formatPhone from "../utils/formatPhone";
 
-export default function LinkGenerator(){
-  const [phoneNumber, setPhoneNumber] = useState('');
+export default function LinkGenerator() {
+  const [phoneNumber, setPhoneNumber] = useState("");
 
-  function handlePhoneInput(e){
-    setPhoneNumber(formatPhone(e.target.value))
+  function handlePhoneInput(e) {
+    setPhoneNumber(formatPhone(e.target.value));
   }
 
-  return <div className="linkGenerator">
-    <h1><i className="bi bi-chat"></i> Gerador de Links</h1>
-    <form action="">
-      <label htmlFor="phoneNumber">Número do WhatsApp</label>
-      <i className="bi bi-telephone"></i>
-      <input type="text" name="phoneNumber" id="phoneNumber" value={phoneNumber} onChange={e => handlePhoneInput(e)}/>
-      <label htmlFor="message">Mensagem (opcional)</label>
-      <textarea name="message" id="message"></textarea>
-      <button type="submit"><i className="bi bi-chat"></i> Preparar Mensagem</button>
-    </form>
-    <div>
-      <p>Link gerado:</p>
-      <input type="text" id="whatsappLink"/>
-      <button id="copyLink"><i className="bi bi-copy"></i></button>
-      <button><i className="bi bi-chat"></i> Abrir WhatsApp</button>
+  return (
+    <div className="linkGenerator">
+      <h1>
+        <i className="bi bi-chat"></i> Gerador de Links
+      </h1>
+      <form action="">
+        <label htmlFor="phoneNumber">Número do WhatsApp</label>
+        <i className="bi bi-telephone"></i>
+        <input
+          type="text"
+          name="phoneNumber"
+          id="phoneNumber"
+          value={phoneNumber}
+          onChange={(e) => handlePhoneInput(e)}
+        />
+        <label htmlFor="message">Mensagem (opcional)</label>
+        <textarea name="message" id="message"></textarea>
+        <button type="submit">
+          <i className="bi bi-chat"></i> Preparar Mensagem
+        </button>
+      </form>
+      <div>
+        <p>Link gerado:</p>
+        <input type="text" id="whatsappLink" />
+        <button id="copyLink">
+          <i className="bi bi-copy"></i>
+        </button>
+        <button>
+          <i className="bi bi-chat"></i> Abrir WhatsApp
+        </button>
+      </div>
     </div>
-  </div>
+  );
 }
