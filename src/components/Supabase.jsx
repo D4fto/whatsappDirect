de3f1593b0a727e3e0681d9d4c7b668362aa9ffb
BrunoBALA <brunoshiba42@gmail.com>
2025-09-19 09:15:07 -0300

    conexão com o supabase
    conexão com o supabase
    criação das funções, adicionar e ler contatos
    configuração das variáveis de ambiente

---
+++ src/components/Supabase.jsx
@@ -0,0 +1,22 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://ioerfhlwmneorwjxrvwg.supabase.co'
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+//função para a criação de um contato
+async function createContact(name, number){
+    const { error } = await supabase
+  .from('contacts')
+  .insert({ name: name ,phone_number: number })
+  return(error)
+}
+//função para a leitura de contatos
+async function readContacts(){
+    const { data, error } = await supabase
+  .from('contacts')
+  .select() 
+  return({data, error})
+}
+
+export default {createContact, readContacts}
